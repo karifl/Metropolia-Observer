67d744f5e6163c66d917ab9e534f36156ba6a8f8
karifl <lampi_88@hotmail.com>
2020-03-29 22:53:49 +0300

    Add files via upload

---
+++ ClockTimer.java
@@ -0,0 +1,45 @@
+package observer;
+
+public class ClockTimer extends Subject {
+	
+	private int secondLimit = 59;
+	private int minuteLimit = 59;
+	private int hourLimit = 24;
+	private int hour=0;
+	private int minute=0;
+	private int second=0;
+	
+	public int getHour() {
+		return hour;
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+	
+	public int getSecond() {
+		return second;
+	}
+	
+	void tick() {
+		
+		if(second >= secondLimit) {
+			second = 0;
+			if(minute >= minuteLimit ) {
+				hour++;
+				minute = 0;
+				if(hour >= hourLimit) {
+					hour=0;
+				}
+				
+			} else {
+				minute++;
+			}
+		} else {
+			second++;
+		}
+		
+		notifyObservers();
+	}
+
+}
---
+++ DigitalClock.java
@@ -0,0 +1,31 @@
+package observer;
+
+public class DigitalClock implements Observer {
+
+	private ClockTimer timer;
+	
+	public DigitalClock(ClockTimer ct) {
+		timer = ct;
+		timer.attach(this);
+	}
+	
+	
+	@Override
+	public void update(Subject s) {
+		// TODO Auto-generated method stub
+		if(s == timer) {
+			draw();
+		}
+	}
+
+
+	private void draw() {
+		// TODO Auto-generated method stub
+		int hour = timer.getHour();
+		int minute = timer.getMinute();
+		int second = timer.getSecond();
+		
+		System.out.println(hour +" : "+minute + " : " +second);
+	}
+
+}
